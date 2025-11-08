d3274df7b0fcb7f9a30d2158ee55db5b3c2f76b4
Nightfly <nightfly@email.ua>
2025-11-08 23:06:58 +0200

    human readable cli states exporter

---
+++ private/states2human.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once(__DIR__ . '/../private/uastates.php');
+
+//converting defaultStates to human readable format
+if (is_array($defaultStates) and count($defaultStates) > 0) {
+    foreach ($defaultStates as $state) {
+        print('+ ' . $state['name'] . PHP_EOL);
+        if (isset($state['districts']) and is_array($state['districts'])) {
+            foreach ($state['districts'] as $district) {
+                print('     - ' . $district['name'] . PHP_EOL);
+            }
+        }
+        if (isset($state['community']) and is_array($state['community'])) {
+            foreach ($state['community'] as $community) {
+                print('         ~ ' . $community['name'] . PHP_EOL);
+            }
+        }
+        print(PHP_EOL);
+    }
+}
